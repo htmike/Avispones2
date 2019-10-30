54235eec9aa07b927792daa2eb0ad97f63f62cf7
Miguel Angel de la Garza Castañuela <miguel.delagarza@banregio.com>
2019-10-30 17:46:55 -0600

    se ajusta servicio score

--- projects/avispones2/src/app/services/score.service.ts
+++ projects/avispones2/src/app/services/score.service.ts
@@ -1,18 +1,37 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Team } from '../modules/screen/classes/team';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 
 })
 export class ScoreService {
-
-  constructor() { }
-
-  getScore( team: string ) {
-    return;
+  mainUrl: string;
+  constructor( private http: HttpClient ) {
+    this.mainUrl = 'http://localhost:7373/';
   }
 
-  update( id: string ) {
-    return;
+  getScoreById( id: number ): Observable<Team> {
+    return this.http.get<Team>(this.mainUrl + 'score/' + id);
+  }
+
+  updateScoreById( id: number, body: any ): any {
+    const headers = new HttpHeaders({'Content-type': 'application/json'});
+    return this.http.post<Team>(this.mainUrl + 'score/' + id, body, { headers }).subscribe( response => {
+      return response;
+    });
+  }
+
+  getExtra() {
+    return this.http.get<Team>(this.mainUrl + 'data');
+  }
+
+  updateExtra( body: any) {
+    const headers = new HttpHeaders({'Content-type': 'application/json'});
+    return this.http.post( this.mainUrl + 'data', body, {headers}).subscribe( response => {
+      return response;
+    });
   }
 }
