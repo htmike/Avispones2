ed4ca16af31b2ee55afd4fb7e78e7e92e81a6ad7
Miguel Angel de la Garza Castañuela <miguel.delagarza@banregio.com>
2019-10-30 17:47:16 -0600

    ajustes generales

--- projects/avispones2/src/app/app-routing.module.ts
+++ projects/avispones2/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'pantalla', loadChildren: () => import ('./modules/screen/screen.module').then( m => m.ScreenModule) },
+  { path: '', redirectTo: '/control', pathMatch: 'full' },
   { path: 'control', loadChildren: () => import ('./modules/control/control.module').then( m => m.ControlModule) },
+  { path: 'pantalla', loadChildren: () => import ('./modules/screen/screen.module').then( m => m.ScreenModule) }
 ];
 
 @NgModule({
