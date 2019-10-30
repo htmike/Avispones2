bd8dde0c4150b2dc0b944866e41f9459add85422
Miguel Angel de la Garza Castañuela <miguel.delagarza@banregio.com>
2019-10-30 15:16:18 -0600

    pruebas con servidor

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const server = express()
+
+const fs = require('file-system')
+var bodyParser = require('body-parser')
+
+server.use(bodyParser.json({limit: '50mb'}))
+server.use(bodyParser.urlencoded({ limit: '500mb', extended: true }))
+
+const PORT = 7373
+
+server.use( (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Request-With, content-type')
+    next()
+  })
+
+server.get('/score/:id', (req, res, next) => {
+    const id = req.params.id
+    const filePath = './server/score-' + id + '.json'
+    if ( fs.fs.existsSync(filePath) ) {
+        const score = fs.readFileSync(filePath, 'utf-8')
+        res.end(score)
+    } else {
+        res.end('Error al consultar score')
+    }
+    next();
+})
+server.post('/score/:id', (req, res, next) => {
+    const id = req.params.id
+    const score = JSON.stringify(req.body)
+    const filePath = './server/score-' + id + '.json'
+    fs.writeFileSync(filePath, score)
+    console.log(score)
+    next()
+})
+server.get('/data', (req, res, next) => {
+    const filePath = './server/data.json'
+    if ( fs.fs.existsSync(filePath) ) {
+        const score = fs.readFileSync(filePath, 'utf-8')
+        res.end(score)
+    } else {
+        res.end('Error al consultar score')
+    }
+    next();
+})
+server.post('/data', (req, res, next) => {
+    const score = JSON.stringify(req.body)
+    const filePath = './server/data.json'
+    fs.writeFileSync(filePath, score)
+    console.log(score)
+    next()
+})
+
+server.listen(PORT, () => {
+    console.log('Works at ' + PORT)
+})
