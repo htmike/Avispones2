009582ce908f5b0007bee959fbb446c5ae56deac
Miguel Angel de la Garza Castañuela <miguel.delagarza@banregio.com>
2019-11-01 12:42:38 -0600

    Se configura electron

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const server = express()
 
+const electron = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+var path = require('path')
+
+
 const fs = require('file-system')
 var bodyParser = require('body-parser')
 
 server.use(bodyParser.json({limit: '50mb'}))
 server.use(bodyParser.urlencoded({ limit: '500mb', extended: true }))
+server.use(express.static('dist/avispones2'))
 
 const PORT = 7373
 
@@ -58,7 +64,63 @@ server.post('/data', (req, res, next) => {
     next()
     res.end()
 })
+server.get('/timer', (req, res, next) => {
+    res.status(200)
+    const filePath = './server/timer.json'
+    if ( fs.fs.existsSync(filePath) ) {
+        const timer = fs.readFileSync(filePath, 'utf-8')
+        res.end(timer)
+    } else {
+        res.end('Error al consultar timer')
+    }
+    next()
+})
+server.post('/timer', (req, res, next) => {
+    res.status(200)
+    const timer = JSON.stringify(req.body)
+    const filePath = './server/timer.json'
+    fs.writeFileSync(filePath, timer)
+    next()
+    res.end()
+})
+
 
 server.listen(PORT, () => {
     console.log('Works at ' + PORT)
 })
+
+// App
+
+electron.app.on('browser-window-created', (e,window) => {
+    let menu = new Menu();
+    window.setMenuBarVisibility(false);
+});
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        fullscreenable: true,
+        width: 384,
+        height: 500,
+        backgroundColor: '#111',
+        icon: path.join(__dirname, 'assets/logo.png')
+    });
+    win.loadURL('http://localhost:4200')
+    win.setMenuBarVisibility(false)
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+});
+app.on('activate', function () {
+    if (win === null) {
+        createWindow();
+    }
+});
