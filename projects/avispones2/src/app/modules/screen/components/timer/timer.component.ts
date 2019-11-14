368c5c8bf46305bff279fa25d70623144d986daf
Miguel Angel de la Garza Castañuela <miguel.delagarza@banregio.com>
2019-11-14 14:43:49 -0600

    se configura app para funcionar con localstorage

--- projects/avispones2/src/app/modules/screen/components/timer/timer.component.ts
+++ projects/avispones2/src/app/modules/screen/components/timer/timer.component.ts
@@ -8,16 +8,8 @@ import { TimerService } from 'projects/avispones2/src/app/services/timer.service
   styleUrls: ['./timer.component.sass']
 })
 export class TimerComponent {
-  timer: Timer;
 
-  constructor( private timerService: TimerService ) {
-    this.timer = new Timer();
-    setInterval( () => {
-      timerService.getTimer().subscribe( data => {
-        this.timer.minutes = data.timer.split(':')[0];
-        this.timer.seconds = data.timer.split(':')[1];
-      });
-    }, 500);
+  constructor( public timerService: TimerService ) {
   }
 
 
